d2cdbd0534c36d4237e09c0d37aa1fc5a21508a4
Dev-Roshika <roshikanayanadhara@gmail.com>
2024-02-21 19:30:57 +0530

    add function add_ab

--- add_num.js
+++ add_num.js
@@ -14,3 +14,15 @@ function add_dynamic(...nums) { // ...nums is a rest parameter that collects all
 }
 
 add_dynamic(1, 2, 3, 4, 5, 6, 7, 8, 9, 10); // 55
+
+function add_ab(a, b){
+  console.log(a + b);
+}
+
+add_ab(1, 2); // 3
+add_ab(1); // NaN
+add_ab(1.23, 2.12); // 3.35
+add_ab("hello", "world"); // helloworld
+// The data types are determined by the values of the arguments that are being passed to the function.
+// The function add_ab is not type safe. It can accept any type of arguments and perform the operation. 
+// This is not a good practice. We should always make sure that the function is type safe.
